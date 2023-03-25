c2f43633f5e2a30fc85f4b0d1bf02ea977ccd2b2
Iroh-Omolola <irohomolola@gmail.com>
2023-03-25 13:37:04 +0100

    create room

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,16 +4,19 @@ import { IoIosPeople } from "react-icons/io";
 const Header = ({createRoom, room, handleRoomChange }) => {
   return (
     <div className="flex items-center justify-between px-4 py-8 bg-background_color">
-      <div className="flex items-center space-x-5">
+      <div className="flex items-center space-x-5 sm:space-x-8">
         <span className="text-[white] text-[14px] sm:text-[15px]">
           Chat App
         </span>
-        <div className="cursor-pointer flex flex-col justify-center" onClick={createRoom}>
-          <h4 className="  text-[10px] sm:text-[14px]">Create Rooms</h4>
-          <IoIosPeople className="w-8 h-8 text-center" />
+        <div
+          className="cursor-pointer flex flex-col justify-center"
+          onClick={createRoom}
+        >
+          <h4 className="text-[white]">Create Rooms</h4>
+          <IoIosPeople className="w-8 h-8 flex justify-center text-center" />
         </div>
         <div className="ml-4">
-          <h2 className="text-[white]">Chat Rooms:</h2>
+          <h2 className="text-[white]">Chat Rooms</h2>
           <select
             value={room}
             onChange={handleRoomChange}
